8cb6f71b6d2da8f2655b2f29bf496dd25e5aefaf
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2018-06-18 15:32:41 +0900

    Fix build script

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ case "$(dpkg --print-architecture)" in
         awk '$3!="debug" {print}' ../${PACKAGE}_${VERSION}_amd64.changes > $TARGET_DIR/${PACKAGE}_${VERSION}_amd64.changes
         mv -f ../${PACKAGE}*_${VERSION_BASE}*.buildinfo ../${PACKAGE}*_${VERSION_BASE}*.deb $TARGET_DIR
         mv -f ../${PACKAGE}*_${VERSION_BASE}*.dsc ../${PACKAGE}*_${VERSION_BASE}*.debian.tar.* $TARGET_DIR
-        cp -f ../${PACKAGE}*_${VERSION_BASE}*.orig.tar.gz $TARGET_DIR
+        cp -f ../${PACKAGE}*_${VERSION_BASE}*.tar.gz $TARGET_DIR
         ;;
     i386)
         dpkg-buildpackage -B -us -uc
