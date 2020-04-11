07691be9fab2a7930ad45baad372877abe434655
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2020-04-11 10:33:54 +0900

    New upstream version 3.4.8

--- setup.sh
+++ setup.sh
@@ -8,17 +8,34 @@ set -x
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 BASE_URL=https://jp-minerals.org/vesta/archives
-case "$(dpkg --print-architecture)" in
-    amd64)
-        wget -O - $BASE_URL/$VERSION_BASE/VESTA-x86_64.tar.bz2 | tar jxf -
-        mv -f VESTA-x86_64 dist
-        ;;
-    i386)
-        echo "32bit OS is not supported"
-        exit 127
-        ;;
-esac
-cp -frp $SCRIPT_DIR/debian $SCRIPT_DIR/vesta $SCRIPT_DIR/vesta.desktop $BUILD_DIR
+if [ $(lsb_release -c -s) = "jessie" -o $(lsb_release -c -s) = "stretch" -o $(lsb_release -c -s) = "xenial" ]; then
+    case "$(dpkg --print-architecture)" in
+        amd64)
+            wget -O - $BASE_URL/$VERSION_BASE/VESTA-gtk2.tar.bz2 | tar jxf -
+            mv -f VESTA-gtk2 dist
+            ;;
+        i386)
+            echo "32bit OS is not supported"
+            exit 127
+            ;;
+    esac
+    cp -frp $SCRIPT_DIR/debian $BUILD_DIR
+    cp -frp $SCRIPT_DIR/debian9/* $BUILD_DIR/debian
+    cp -frp $SCRIPT_DIR/vesta $SCRIPT_DIR/vesta.desktop $BUILD_DIR
+else
+    case "$(dpkg --print-architecture)" in
+        amd64)
+            wget -O - $BASE_URL/$VERSION_BASE/VESTA-gtk3.tar.bz2 | tar jxf -
+            mv -f VESTA-gtk3 dist
+            ;;
+        i386)
+            echo "32bit OS is not supported"
+            exit 127
+            ;;
+    esac
+    cp -frp $SCRIPT_DIR/debian $BUILD_DIR
+    cp -frp $SCRIPT_DIR/vesta $SCRIPT_DIR/vesta.desktop $BUILD_DIR
+fi
 
 cd $BUILD_DIR
 sudo apt-get update
