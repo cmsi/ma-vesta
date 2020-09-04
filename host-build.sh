5be62e2185aeea91c225981e6fcb9eeb12d9819d
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2020-09-04 15:39:12 +0900

    New upstream version 3.5.3

--- host-build.sh
+++ host-build.sh
@@ -2,7 +2,7 @@
 . $(dirname $0)/path.sh
 
 VM="$*"
-test -z "$VM" && VM="madev10-amd64 madev10-i386 madev9-amd64 madev9-i386 madev8-amd64 madev8-i386 ubuntu20-amd64 ubuntu18-amd64 ubuntu16-amd64"
+test -z "$VM" && VM="madev10-amd64 ubuntu20-amd64 ubuntu18-amd64"
 for v in $VM; do
   cd $HOME/vagrant/$v
   vagrant ssh -c "sh development/ma-${PACKAGE}/setup.sh"
