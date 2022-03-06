8ab88bffbea5c6dcc638ca42864a556cae914bd0
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2022-03-06 18:55:44 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
 . $(dirname $0)/path.sh
-test -z $BUILD_DIR && exit 127
-
-rm -rf $BUILD_DIR
 set -x
 
-mkdir -p $BUILD_DIR
-cd $BUILD_DIR
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
 BASE_URL=https://jp-minerals.org/vesta/archives
-
-wget -O - $BASE_URL/$VERSION_BASE/VESTA-gtk3.tar.bz2 | tar jxf -
+wget -O - ${BASE_URL}/${VERSION_BASE}/VESTA-gtk3.tar.bz2 | tar jxf -
 mv -f VESTA-gtk3 dist
 
-cp -frp $SCRIPT_DIR/debian $BUILD_DIR
-cp -frp $SCRIPT_DIR/vesta $SCRIPT_DIR/vesta.desktop $BUILD_DIR
+cd ${BUILD_DIR}
+mkdir -p debian
+cp -rp ${SCRIPT_DIR}/debian/* debian/
+cp -rp ${SCRIPT_DIR}/vesta ${SCRIPT_DIR}/vesta.desktop $BUILD_DIR
 
-cd $BUILD_DIR
-sudo apt-get update
-sudo apt-get -y upgrade
+apt-get update
+apt-get -y upgrade
 dpkg-checkbuilddeps 2>&1 | sed 's/dpkg-checkbuilddeps.*dependencies: //' | xargs sudo apt-get -y install
